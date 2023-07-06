b00f7deddad2a2c8b91f24a890b99894356b07f8
Warren <99427649+kobe-eth@users.noreply.github.com>
2023-07-06 12:09:51 +0200

    fix: solc version ^ instead of >=

--- src/base/interfaces/ILocker.sol
+++ src/base/interfaces/ILocker.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.7;
+pragma solidity ^0.8.7;
 
 interface ILocker {
     function createLock(uint256, uint256) external;
--- src/base/interfaces/ITokenMinter.sol
+++ src/base/interfaces/ITokenMinter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.7;
+pragma solidity ^0.8.7;
 
 interface ITokenMinter {
     function mint(address, uint256) external;
