c675ff7a6aecebbaf5e24735ef905b2ad4660bc7
PumpkingWok <andrearanma@gmail.com>
2023-10-18 15:16:49 +0200

    add: abstract strategy

--- src/base/interfaces/ILiquidityGaugeStrat.sol
+++ src/base/interfaces/ILiquidityGaugeStrat.sol
@@ -2,12 +2,21 @@
 pragma solidity 0.8.19;
 
 interface ILiquidityGaugeStrat {
+    // solhint-disable-next-line
+    function add_reward(address _token, address _distributor) external;
+
+    function balanceOf(address _user) external view returns (uint256);
+
+    // solhint-disable-next-line
+    function claim_rewards(address _user) external;
+
     function deposit(uint256 _value, address _user) external;
 
     // solhint-disable-next-line
     function deposit_reward_token(address _rewardToken, uint256 _amount) external;
 
-    function withdraw(uint256 _value, address _user, bool _claim) external;
+    // solhint-disable-next-line
+    function reward_tokens(uint256 _i) external view returns (address);
 
-    function balanceOf(address _user) external view returns (uint256);
+    function withdraw(uint256 _value, address _user, bool _claim) external;
 }
--- src/base/interfaces/ILocker.sol
+++ src/base/interfaces/ILocker.sol
@@ -2,5 +2,13 @@
 pragma solidity 0.8.19;
 
 interface ILocker {
+    function execute(address _to, uint256 _value, bytes calldata _data) external returns (bool, bytes memory);
+
+    function increaseAmount(uint256 _amount) external;
+
+    function release() external;
+
+    function setGovernance(address _gov) external;
+
     function setStrategy(address _strategy) external;
 }
