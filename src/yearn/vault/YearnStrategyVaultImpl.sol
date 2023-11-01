d2b131fcb46334c0807704cddbd104bca4356f52
Warren <99427649+kobe-eth@users.noreply.github.com>
2023-11-01 15:02:14 +0100

    fix: remove unecessary override

--- src/yearn/vault/YearnStrategyVaultImpl.sol
+++ src/yearn/vault/YearnStrategyVaultImpl.sol
@@ -6,12 +6,7 @@ import "src/base/vault/StrategyVaultImpl.sol";
 /// @notice Vault implementation for Stake DAO.
 /// @dev Deposit LP tokens to Stake DAO and receive sdGauge tokens as a receipt.
 contract YearnStrategyVaultImpl is StrategyVaultImpl {
-    /// @notice Initialize the Yearn vault
-    function initialize() public override {
-        super.initialize();
-    }
-
-    /// @notice Deposit the yearn gauge token
+      /// @notice Deposit the yearn gauge token
     /// @param _receiver address to deposit for
     /// @param _amount amount to deposit
     function depositGaugeToken(address _receiver, uint256 _amount) external {
