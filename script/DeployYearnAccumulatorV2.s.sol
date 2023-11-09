19cd01b04678525d11aeee7296d112cac55b101d
PumpkingWok <andrearanma@gmail.com>
2023-11-09 14:52:02 +0100

    feature: yearn acc v2 deploy script

---
+++ script/DeployYearnAccumulatorV2.s.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.19;
+
+import "forge-std/Test.sol";
+import "forge-std/Script.sol";
+
+import {AddressBook} from "@addressBook/AddressBook.sol";
+import {YearnAccumulatorV2} from "src/yearn/accumulator/YearnAccumulatorV2.sol";
+
+contract DeployYearnAccumulatorV2 is Script, Test {
+    address public constant DEPLOYER = 0x000755Fbe4A24d7478bfcFC1E561AfCE82d1ff62;
+    address public constant DAO_FEE_RECIPIENT = 0xF930EBBd05eF8b25B1797b9b2109DDC9B0d43063;
+    address public constant LIQUIDITY_FEE_RECIPIENT = 0xF930EBBd05eF8b25B1797b9b2109DDC9B0d43063;
+    address public constant YEARN_STRATEGY = 0x1be150a35bb8233d092747eBFDc75FB357c35168;
+
+    address public sdYfiLG;
+    address public yfiLocker;
+
+    YearnAccumulatorV2 public accumulator;
+
+    function run() public {
+        vm.startBroadcast(DEPLOYER);
+        sdYfiLG = AddressBook.GAUGE_SDYFI;
+        yfiLocker = AddressBook.YFI_LOCKER;
+        accumulator =
+            new YearnAccumulatorV2(sdYfiLG, yfiLocker, DAO_FEE_RECIPIENT, LIQUIDITY_FEE_RECIPIENT, YEARN_STRATEGY);
+        vm.stopBroadcast();
+    }
+}
