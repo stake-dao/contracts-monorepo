72f592b96cd529017ab2f2e0f40ac6f1c242a257
Warren <99427649+kobe-eth@users.noreply.github.com>
2023-12-05 17:13:49 +0100

    fix: cake accumulator deployment scropt

--- script/DeployCakeAccumulator.s.sol
+++ script/DeployCakeAccumulator.s.sol
@@ -13,6 +13,7 @@ contract DeployCakeAccumulator is Script {
         0x9cac9745731d1Cf2B483f257745A512f0938DD01 // veCAKe emission
     ];
 
+    address public constant DEPLOYER = 0x000755Fbe4A24d7478bfcFC1E561AfCE82d1ff62;
     address public constant GOVERNANCE = 0xB0552b6860CE5C0202976Db056b5e3Cc4f9CC765;
 
     address public constant CAKE = AddressBook.CAKE;
@@ -21,8 +22,12 @@ contract DeployCakeAccumulator is Script {
     address public constant RSPG = 0x011f2a82846a4E9c62C2FC4Fd6fDbad19147D94A;
     address public constant EXTRA_REWARD = 0x4DB5a66E937A9F4473fA95b1cAF1d1E1D62E29EA;
 
-    function setUp() public virtual {
+    function run() public {
+        vm.startBroadcast(DEPLOYER);
+
         accumulator =
             new CakeAccumulator(SD_CAKE_GAUGE, CAKE_LOCKER, GOVERNANCE, GOVERNANCE, GOVERNANCE);
+
+        vm.stopBroadcast();
     }
 }
