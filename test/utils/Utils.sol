8f918135b50f2b364f9760bdd42239624ff69e39
PumpkingWok <andrearanma@gmail.com>
2024-04-04 12:44:59 +0200

    chore: removed unused functions in Utils

--- test/utils/Utils.sol
+++ test/utils/Utils.sol
@@ -15,29 +15,4 @@ library Utils {
 
         if (deployed == address(0)) revert DEPLOYMENT_FAILED();
     }
-
-    function deployCreate2(bytes32 salt, bytes memory bytecode) external returns (address deployed) {
-        assembly {
-            deployed := create2(0, add(bytecode, 0x20), mload(bytecode), salt)
-        }
-
-        if (deployed == address(0)) {
-            revert DEPLOYMENT_FAILED();
-        }
-    }
-
-    function computeAddress(bytes32 salt, bytes32 creationCodeHash) external view returns (address addr) {
-        address contractAddress = address(this);
-
-        assembly {
-            let ptr := mload(0x40)
-
-            mstore(add(ptr, 0x40), creationCodeHash)
-            mstore(add(ptr, 0x20), salt)
-            mstore(ptr, contractAddress)
-            let start := add(ptr, 0x0b)
-            mstore8(start, 0xff)
-            addr := keccak256(start, 85)
-        }
-    }
 }
