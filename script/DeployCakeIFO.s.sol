c0dc5ecc032bc7e93807c05f923a4b5c45f48582
PumpkingWok <andrearanma@gmail.com>
2024-05-08 12:40:41 +0200

    feat: CakeIFOFactory deploy script

---
+++ script/DeployCakeIFO.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.19;
+
+import "forge-std/Script.sol";
+import {CakeIFOFactory} from "src/cake/ifo/CakeIFOFactory.sol";
+import {CAKE} from "address-book/lockers/56.sol";
+import {DAO} from "address-book/dao/56.sol";
+
+contract DeployFXNAccumulator is Script {
+
+    CakeIFOFactory private factory;
+
+    function run() public {
+        vm.startBroadcast(DAO.MAIN_DEPLOYER);
+        // it needs to be runned on bnb network
+        require(block.chainid == 56, "Wrong network");
+
+        // set feeReceiver as governance at deploy time
+        factory = new CakeIFOFactory(CAKE.LOCKER, CAKE.EXECUTOR, DAO.GOVERNANCE, DAO.GOVERNANCE);
+
+        vm.stopBroadcast();
+    }
+}
