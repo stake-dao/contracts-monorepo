ea361ee65a2984fe71ae44e03e2fbb0833d8bf89
PumpkingWok <andrearanma@gmail.com>
2024-06-18 16:46:18 +0200

    feat: FxsAccumulatorV2Fraxtal deploy script

---
+++ script/DeployFxsAccumulatorV2Fraxtal.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.19;
+
+import "forge-std/Script.sol";
+import {FxsAccumulatorV2Fraxtal} from "src/frax/fxs/accumulator/FxsAccumulatorV2Fraxtal.sol";
+
+contract DeployFxsAccumulatoV2Fraxtal is Script {
+    FxsAccumulatorV2Fraxtal public accumulator;
+
+    address public constant GAUGE = 0x12992595328E52267c95e45B1a97014D6Ddf8683;
+    address public constant LOCKER = 0x26aCff2adc9104FE1c26c958dC4C9a5180840c35;
+    address public constant GOVERNANCE = 0xB0552b6860CE5C0202976Db056b5e3Cc4f9CC765;
+    address internal constant DELEGATION_REGISTRY = 0xF5cA906f05cafa944c27c6881bed3DFd3a785b6A;
+    address internal constant DEPLOYER = 0x000755Fbe4A24d7478bfcFC1E561AfCE82d1ff62;
+
+    function run() public {
+        vm.startBroadcast(DEPLOYER);
+
+        require(block.chainid == 252, "wrong network");
+        accumulator = new FxsAccumulatorV2Fraxtal(GAUGE, LOCKER, GOVERNANCE, GOVERNANCE, GOVERNANCE, DELEGATION_REGISTRY, GOVERNANCE);
+
+        vm.stopBroadcast();
+    }
+}
