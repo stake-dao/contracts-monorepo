0fcf3aa487dbf0d58ac66a4ad993243a8f8f0b5e
Warren <99427649+kobe-eth@users.noreply.github.com>
2024-07-01 19:00:52 +0200

    feat: add fxs acc tests

--- packages/herdaddy/src/interfaces/frax/IFraxShares.sol
+++ packages/herdaddy/src/interfaces/frax/IFraxShares.sol
@@ -2,5 +2,7 @@
 pragma solidity ^0.8.0;
 
 interface IFraxShares {
+
+    function owner_address() external view returns (address);
     function toggleVotes() external;
 }
--- packages/lockers/test/frax/FXSAccumulatorV3IntegrationTest.sol
+++ packages/lockers/test/frax/FXSAccumulatorV3IntegrationTest.sol
@@ -2,16 +2,16 @@
 pragma solidity ^0.8.4;
 
 import "test/base/AccumulatorV3.t.sol";
-import "herdaddy/src/interfaces/frax/IFraxShares.sol";
+import "herdaddy/interfaces/frax/IFraxShares.sol";
 import "src/frax/fxs/accumulator/FXSAccumulatorV3.sol";
 
 contract FXSAccumulatorV3IntegrationTest is AccumulatorV2Test {
     constructor() AccumulatorV2Test(20_187_867, FXS.LOCKER, FXS.SDTOKEN, Frax.VEFXS, FXS.GAUGE, FXS.TOKEN, FXS.TOKEN) {}
 
     function _deployAccumulator() internal override returns (address payable) {
+        /// Disable tracking votes in order to mint any amount of FXS for testing purposes.
+        vm.prank(IFraxShares(FXS.TOKEN).owner_address());
         IFraxShares(FXS.TOKEN).toggleVotes();
-
-
 
         return payable(new FXSAccumulatorV3(address(liquidityGauge), locker, address(this)));
     }
