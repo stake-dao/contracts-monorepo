1f2cffcfde87b11789ee0fdcf802788b90f3ab69
Warren <99427649+kobe-eth@users.noreply.github.com>
2024-07-01 17:25:15 +0200

    feat: complete fx tests

--- packages/lockers/test/fx/FXNAccumulatorV3IntegrationTest.sol
+++ packages/lockers/test/fx/FXNAccumulatorV3IntegrationTest.sol
@@ -15,7 +15,7 @@ import {ILocker} from "src/base/interfaces/ILocker.sol";
 import {ILiquidityGauge} from "src/base/interfaces/ILiquidityGauge.sol";
 
 contract FXNAccumulatorV3IntegrationTest is Test {
-    uint256 blockNumber = 20_031_924;
+    uint256 blockNumber = 20_187_797;
 
     ERC20 public WSTETH = ERC20(0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0);
 
@@ -76,5 +76,74 @@ contract FXNAccumulatorV3IntegrationTest is Test {
 
         assertEq(split.fees[0], 500);
         assertEq(split.fees[1], 1000);
+    }
+
+    function test_claimAll() public {
+        //Check Dao recipient
+        assertEq(WSTETH.balanceOf(address(treasuryRecipient)), 0);
+        //// Check Bounty recipient
+        assertEq(WSTETH.balanceOf(address(liquidityFeeRecipient)), 0);
+        //// Check lgv4
+        uint256 gaugeBalanceBefore = WSTETH.balanceOf(address(liquidityGauge));
+
+        accumulator.claimAndNotifyAll(false, false, false);
+
+        uint256 treasury = WSTETH.balanceOf(address(treasuryRecipient));
+        uint256 liquidityFee = WSTETH.balanceOf(address(liquidityFeeRecipient));
+        uint256 gauge = WSTETH.balanceOf(address(liquidityGauge)) - gaugeBalanceBefore;
+        uint256 claimer = WSTETH.balanceOf(address(this));
+
+        /// WETH is distributed over 4 weeks to gauge.
+        uint256 remaining = WSTETH.balanceOf(address(accumulator));
+        uint256 total = treasury + liquidityFee + gauge + claimer + remaining;
+
+        FXNAccumulatorV3.Split memory feeSplit = accumulator.getFeeSplit();
+
+        assertEq(total * accumulator.claimerFee() / 10_000, claimer);
+
+        assertEq(total * feeSplit.fees[0] / 10_000, treasury);
+        assertEq(total * feeSplit.fees[1] / 10_000, liquidityFee);
+
+        skip(1 weeks);
+
+        uint256 _before = ERC20(FXN.TOKEN).balanceOf(address(liquidityGauge));
+
+        deal(FXN.TOKEN, address(accumulator), 1_000_000e18);
+
+        accumulator.approveNewTokenReward(FXN.TOKEN);
+        accumulator.notifyReward(address(FXN.TOKEN), false, false);
+
+        /// It should distribute 1_000_000 PENDLE to LGV4, meaning no fees were taken.
+        assertEq(ERC20(FXN.TOKEN).balanceOf(address(liquidityGauge)), _before + 1_000_000e18);
+    }
+
+    function test_setters() public {
+        address[] memory feeSplitReceivers = new address[](2);
+        uint256[] memory feeSplitFees = new uint256[](2);
+
+        feeSplitReceivers[0] = address(treasuryRecipient);
+        feeSplitFees[0] = 100; // 5% to dao
+
+        feeSplitReceivers[1] = address(liquidityFeeRecipient);
+        feeSplitFees[1] = 50; // 5% to liquidity
+
+        accumulator.setFeeSplit(feeSplitReceivers, feeSplitFees);
+        FXNAccumulatorV3.Split memory split = accumulator.getFeeSplit();
+
+        assertEq(split.receivers[0], address(treasuryRecipient));
+        assertEq(split.receivers[1], address(liquidityFeeRecipient));
+
+        assertEq(split.fees[0], 100);
+        assertEq(split.fees[1], 50);
+
+        accumulator.setClaimerFee(1000);
+        assertEq(accumulator.claimerFee(), 1000);
+
+        accumulator.setFeeReceiver(address(1));
+        assertEq(accumulator.feeReceiver(), address(1));
+
+        vm.prank(address(2));
+        vm.expectRevert(AccumulatorV2.GOVERNANCE.selector);
+        accumulator.setFeeReceiver(address(2));
     }
 }
