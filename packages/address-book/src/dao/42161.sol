f0aa30aa9a5c456b085d1400418202b003ace1c7
Warren <99427649+kobe-eth@users.noreply.github.com>
2024-09-17 17:26:03 +0200

    feat: add ifo arb wip

--- packages/address-book/src/dao/42161.sol
+++ packages/address-book/src/dao/42161.sol
@@ -3,4 +3,5 @@ pragma solidity >=0.6.0;
 
 library DAO {
     address public constant GOVERNANCE = 0xfDB1157ac847D334b8912df1cd24a93Ee22ff3d0;
+    address public constant MAIN_DEPLOYER = 0x000755Fbe4A24d7478bfcFC1E561AfCE82d1ff62;
 }
--- packages/lockers/src/common/interfaces/ICakeV3.sol
+++ packages/lockers/src/common/interfaces/ICakeV3.sol
@@ -3,4 +3,5 @@ pragma solidity ^0.8.19;
 
 interface ICakeV3 {
     function getUserCreditWithIfoAddr(address _user, address _ifo) external view returns (uint256);
+    function getUserCredit(address _user) external view returns (uint256);
 }
