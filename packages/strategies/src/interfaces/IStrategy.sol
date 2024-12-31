3e1aebd4d8f72d99b7455b78cd97290becc30075
Warren <99427649+kobe-eth@users.noreply.github.com>
2024-12-31 16:24:30 +0100

    feat: deposit wip

--- packages/strategies/src/interfaces/IStrategy.sol
+++ packages/strategies/src/interfaces/IStrategy.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.19;
 
+import "src/interfaces/IAllocator.sol";
+
 interface IStrategy {
-    function deposit(uint256 assets, address receiver) external returns (uint256);
-    function withdraw(uint256 assets, address receiver, address owner) external returns (uint256);
+    function deposit(IAllocator.Allocation[] memory allocations) external returns (uint256 pendingRewards);
+    function withdraw(IAllocator.Allocation[] memory allocations) external returns (uint256 pendingRewards);
 }
