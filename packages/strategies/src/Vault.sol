49b02bc4676a16e0d513208c9cf4bcf321336878
Warren <99427649+kobe-eth@users.noreply.github.com>
2024-12-31 15:46:34 +0100

    refactor: reward distributor as standalone extension wip

--- packages/strategies/src/Vault.sol
+++ packages/strategies/src/Vault.sol
@@ -1,8 +1,20 @@
 /// SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.19;
 
-import "src/extensions/RewardDistributor.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
-contract Vault is RewardDistributor {
-    constructor(address asset) RewardDistributor(asset) {}
+contract Vault is ERC4626 {
+    address public immutable REWARD_DISTRIBUTOR;
+
+    constructor(address asset, address rewardDistributor)
+        ERC4626(IERC20(asset))
+        ERC20(
+            string.concat("StakeDAO ", IERC20Metadata(asset).symbol(), " Vault"),
+            string.concat("sd-", IERC20Metadata(asset).symbol(), "-vault")
+        )
+    {
+        REWARD_DISTRIBUTOR = rewardDistributor;
+    }
 }
