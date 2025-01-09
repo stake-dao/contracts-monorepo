717b35bef7e760eabbbc0af4c955432afd20cefa
Warren <99427649+kobe-eth@users.noreply.github.com>
2025-01-09 17:27:58 +0100

    chore: more readable slots wip

--- packages/strategies/src/Accountant.sol
+++ packages/strategies/src/Accountant.sol
@@ -6,28 +6,28 @@ import "src/interfaces/IRegistry.sol";
 /// @notice The source of truth.
 contract Accountant {
     /// @notice Packed vault data structure into 2 slots for gas optimization
-    /// @dev Slot 1: [supply (128) | integral (128)]
-    /// @dev Slot 2: [lastUpdateTime (64) | pendingRewards (64)]
+    /// @dev supplyAndIntegralSlot: [supply (128) | integral (128)]
+    /// @dev timeAndRewardsSlot: [lastUpdateTime (64) | pendingRewards (64)]
     struct PackedVault {
-        uint256 slot1;
-        uint128 slot2;
+        uint256 supplyAndIntegralSlot; // slot1 -> supplyAndIntegralSlot
+        uint128 timeAndRewardsSlot; // slot2 -> timeAndRewardsSlot
     }
 
     /// @notice Packed account data structure into 1 slot for gas optimization
-    /// @dev [balance (96) | integral (96) | pendingRewards (64)]
+    /// @dev balanceAndRewardsSlot: [balance (96) | integral (96) | pendingRewards (64)]
     struct PackedAccount {
-        uint256 slot;
+        uint256 balanceAndRewardsSlot; // slot -> balanceAndRewardsSlot
     }
 
-    /// @dev Bit masks for vault slot 1
+    /// @dev Bit masks for vault supplyAndIntegralSlot
     uint256 private constant SUPPLY_MASK = (1 << 128) - 1;
     uint256 private constant INTEGRAL_MASK = ((1 << 128) - 1) << 128;
 
-    /// @dev Bit masks for vault slot 2
+    /// @dev Bit masks for vault timeAndRewardsSlot
     uint128 private constant LAST_UPDATE_TIME_MASK = (1 << 64) - 1;
     uint128 private constant PENDING_REWARDS_MASK = ((1 << 64) - 1) << 64;
 
-    /// @dev Bit masks for account slot
+    /// @dev Bit masks for account balanceAndRewardsSlot
     uint256 private constant BALANCE_MASK = (1 << 96) - 1;
     uint256 private constant ACCOUNT_INTEGRAL_MASK = ((1 << 96) - 1) << 96;
     uint256 private constant ACCOUNT_PENDING_REWARDS_MASK = ((1 << 64) - 1) << 192;
@@ -68,11 +68,11 @@ contract Accountant {
         if (msg.sender != IRegistry(REGISTRY).vaults(asset)) revert OnlyVault();
 
         PackedVault storage _vault = vaults[msg.sender];
-        uint256 vaultSlot1 = _vault.slot1;
-        // uint128 vaultSlot2 = _vault.slot2;
+        uint256 vaultSupplyAndIntegral = _vault.supplyAndIntegralSlot;
+        // uint128 vaultTimeAndRewards = _vault.timeAndRewardsSlot;
 
-        uint256 supply = uint128(vaultSlot1 & SUPPLY_MASK);
-        uint256 integral = uint128((vaultSlot1 & INTEGRAL_MASK) >> 128);
+        uint256 supply = uint128(vaultSupplyAndIntegral & SUPPLY_MASK);
+        uint256 integral = uint128((vaultSupplyAndIntegral & INTEGRAL_MASK) >> 128);
 
         /// 0. Update the vault integral with the pending rewards distributed.
         if (PRE_CHECKPOINT_REWARDS && pendingRewards > 0) {
@@ -86,15 +86,15 @@ contract Accountant {
         /// 2. Transferring. Update the "from" account.
         else {
             PackedAccount storage _from = accounts[msg.sender][from];
-            uint256 fromSlot = _from.slot;
-            uint256 fromBalance = uint96(fromSlot & BALANCE_MASK);
-            uint256 fromIntegral = uint96((fromSlot & ACCOUNT_INTEGRAL_MASK) >> 96);
-            uint256 fromPendingRewards = uint64((fromSlot & ACCOUNT_PENDING_REWARDS_MASK) >> 192);
+            uint256 fromBalanceAndRewards = _from.balanceAndRewardsSlot;
+            uint256 fromBalance = uint96(fromBalanceAndRewards & BALANCE_MASK);
+            uint256 fromIntegral = uint96((fromBalanceAndRewards & ACCOUNT_INTEGRAL_MASK) >> 96);
+            uint256 fromPendingRewards = uint64((fromBalanceAndRewards & ACCOUNT_PENDING_REWARDS_MASK) >> 192);
 
             fromPendingRewards += uint64((integral - fromIntegral) * fromBalance / supply);
             fromBalance -= amount;
 
-            _from.slot = (fromBalance & BALANCE_MASK) | ((integral << 96) & ACCOUNT_INTEGRAL_MASK)
+            _from.balanceAndRewardsSlot = (fromBalance & BALANCE_MASK) | ((integral << 96) & ACCOUNT_INTEGRAL_MASK)
                 | ((fromPendingRewards << 192) & ACCOUNT_PENDING_REWARDS_MASK);
         }
 
@@ -105,15 +105,15 @@ contract Accountant {
         /// 4. Transferring. Update the "to" account.
         else {
             PackedAccount storage _to = accounts[msg.sender][to];
-            uint256 toSlot = _to.slot;
-            uint256 toBalance = uint96(toSlot & BALANCE_MASK);
-            uint256 toIntegral = uint96((toSlot & ACCOUNT_INTEGRAL_MASK) >> 96);
-            uint256 toPendingRewards = uint64((toSlot & ACCOUNT_PENDING_REWARDS_MASK) >> 192);
+            uint256 toBalanceAndRewards = _to.balanceAndRewardsSlot;
+            uint256 toBalance = uint96(toBalanceAndRewards & BALANCE_MASK);
+            uint256 toIntegral = uint96((toBalanceAndRewards & ACCOUNT_INTEGRAL_MASK) >> 96);
+            uint256 toPendingRewards = uint64((toBalanceAndRewards & ACCOUNT_PENDING_REWARDS_MASK) >> 192);
 
             toPendingRewards += uint64((integral - toIntegral) * toBalance / supply);
             toBalance += amount;
 
-            _to.slot = (toBalance & BALANCE_MASK) | ((integral << 96) & ACCOUNT_INTEGRAL_MASK)
+            _to.balanceAndRewardsSlot = (toBalance & BALANCE_MASK) | ((integral << 96) & ACCOUNT_INTEGRAL_MASK)
                 | ((toPendingRewards << 192) & ACCOUNT_PENDING_REWARDS_MASK);
         }
 
@@ -123,14 +123,14 @@ contract Accountant {
         }
 
         // Update vault storage
-        _vault.slot1 = (supply & SUPPLY_MASK) | ((integral << 128) & INTEGRAL_MASK);
+        _vault.supplyAndIntegralSlot = (supply & SUPPLY_MASK) | ((integral << 128) & INTEGRAL_MASK);
     }
 
     function totalSupply(address vault) external view returns (uint256) {
-        return uint128(vaults[vault].slot1 & SUPPLY_MASK);
+        return uint128(vaults[vault].supplyAndIntegralSlot & SUPPLY_MASK);
     }
 
     function balanceOf(address vault, address account) external view returns (uint256) {
-        return uint96(accounts[vault][account].slot & BALANCE_MASK);
+        return uint96(accounts[vault][account].balanceAndRewardsSlot & BALANCE_MASK);
     }
 }
