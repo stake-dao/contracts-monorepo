b2141d262664331785d0664f36ddcda5b30949af
Baptiste Florentin <baptiste.florentin.pro@gmail.com>
2025-01-14 16:56:44 +0100

    anticipated WETH claiming + move claiming logic to the Accumulator
    ZeroLend will add WETH rewards in the near future, we already anticipated this new feature.
    The Accumulator now calls the `execute` function on the locker to have more flexibility to claim fees and avoid having dead source code in the case where ZeroLend would upgrade their contract to a an incompatible version.

--- packages/lockers/src/linea/zerolend/zero/Accumulator.sol
+++ packages/lockers/src/linea/zerolend/zero/Accumulator.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.19;
 
 import "src/common/accumulator/BaseAccumulator.sol";
 import {ILocker} from "src/common/interfaces/ILocker.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /// @notice A contract that accumulates FXN rewards and notifies them to the sdFXN gauge
 /// @author StakeDAO
@@ -10,11 +11,11 @@ contract Accumulator is BaseAccumulator {
     /// @notice ZERO token address.
     address public constant ZERO = 0x78354f8DcCB269a615A7e0a24f9B0718FDC3C7A7;
 
+    // @notice WETH token address.
+    address public constant WETH = 0xe5D7C2a44FfDDf6b295A15c148167daaAf5Cf34f;
+
     /// @notice Fee distributor address.
     address public constant ZERO_VP = 0xf374229a18ff691406f99CCBD93e8a3f16B68888;
-
-    // ZERO token
-    address public constant REWARD_TOKEN = ZERO;
 
     //////////////////////////////////////////////////////
     /// --- CONSTRUCTOR
@@ -27,7 +28,8 @@ contract Accumulator is BaseAccumulator {
     constructor(address _gauge, address _locker, address _governance)
         BaseAccumulator(_gauge, ZERO, _locker, _governance)
     {
-        SafeTransferLib.safeApprove(REWARD_TOKEN, _gauge, type(uint256).max);
+        SafeTransferLib.safeApprove(ZERO, _gauge, type(uint256).max);
+        SafeTransferLib.safeApprove(WETH, _gauge, type(uint256).max);
     }
 
     //////////////////////////////////////////////////////
@@ -38,15 +40,19 @@ contract Accumulator is BaseAccumulator {
     // TODO do we remove the notifySDT variable since we will be using the LiquidityGaugeV4XChain
     //      we could also set it to false directly below?
     function claimAndNotifyAll(bool notifySDT, bool claimFeeStrategy) external override {
-        ILocker(locker).claimRewards(ZERO_VP, REWARD_TOKEN, address(this));
+        // claim rewards from the locker
+        ILocker(locker).execute(ZERO_VP, 0, abi.encodeWithSignature("getReward()"));
 
-        // TODO should remove this right?
-        // /// Claim Extra FXN rewards.
-        // if (claimFeeStrategy && strategy != address(0)) {
-        //     _claimFeeStrategy();
-        // }
+        // transfer rewards to the gauge
+        ILocker(locker).execute(
+            ZERO, 0, abi.encodeWithSignature("transfer(address,uint256)", address(this), IERC20(ZERO).balanceOf(locker))
+        );
+        ILocker(locker).execute(
+            WETH, 0, abi.encodeWithSignature("transfer(address,uint256)", address(this), IERC20(WETH).balanceOf(locker))
+        );
 
-        notifyReward(REWARD_TOKEN, notifySDT, claimFeeStrategy);
+        notifyReward(ZERO, notifySDT, claimFeeStrategy);
+        notifyReward(WETH, notifySDT, claimFeeStrategy);
     }
 
     function name() external pure override returns (string memory) {
