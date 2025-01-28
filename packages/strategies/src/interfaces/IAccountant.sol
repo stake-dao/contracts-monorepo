050d22883b3a8ca5cf949c9a2a6d67ea907aab61
Warren <99427649+kobe-eth@users.noreply.github.com>
2025-01-28 17:19:39 +0100

    feat: add donate packed storage

--- packages/strategies/src/interfaces/IAccountant.sol
+++ packages/strategies/src/interfaces/IAccountant.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.7;
 
 interface IAccountant {
-    function checkpoint(address gauge, address from, address to, uint256 amount, uint256 pendingRewards) external;
+    function checkpoint(address gauge, address from, address to, uint256 amount, uint256 pendingRewards, bool claimed)
+        external;
 
     function totalSupply(address asset) external view returns (uint256);
     function balanceOf(address asset, address account) external view returns (uint256);
