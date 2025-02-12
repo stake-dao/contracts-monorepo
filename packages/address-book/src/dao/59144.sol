1518eb2cb88c52d1252e8486ebf207315599939a
Baptiste Florentin <baptiste.florentin.pro@gmail.com>
2025-02-12 15:57:22 +0100

    feat: add linea governance + refactor DAO library

---
+++ packages/address-book/src/dao/59144.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.6.0;
+
+library DAO {
+    address public constant MAIN_DEPLOYER = 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38; // TODO change?
+    address public constant TREASURY = address(2); // TODO complete
+    address public constant LIQUIDITY_FEES_RECIPIENT = address(3); // TODO complete
+    address public constant GOVERNANCE = 0xB0552b6860CE5C0202976Db056b5e3Cc4f9CC765;
+}
