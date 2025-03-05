7b51fcc8c50458e37da94aa1a1594339729d632a
qd-qd <qdqdqdqdqd@protonmail.com>
2025-03-05 17:29:37 +0100

    ✅ test: Accountant's getPendingRewards method

---
+++ packages/strategies/test/unit/Accountant/getPendingRewards.t.sol
@@ -0,0 +1,42 @@
+pragma solidity 0.8.28;
+
+import {Accountant} from "src/Accountant.sol";
+import {BaseTest} from "test/Base.t.sol";
+import {AccountantHarness} from "test/unit/Accountant/AccountantHarness.t.sol";
+
+contract Accountant__getPendingRewards is BaseTest {
+    function test_ReturnsTheCorrectUserPendingRewardsForSpecificVault(uint128 pendingRewards)
+        external
+        _cheat_replaceAccountantWithAccountantHarness
+    {
+        // it returns the correct user balance for specific vault
+
+        AccountantHarness accountantHarness = AccountantHarness(address(accountant));
+        address user = makeAddr("user");
+
+        // We are putting the contract into a state where the vault has a non-null state
+        // This function is a testing-only function that shortcut the real end-user flow
+        accountantHarness._cheat_updateUserData(
+            vault, user, Accountant.AccountData({balance: 0, integral: 0, pendingRewards: pendingRewards})
+        );
+
+        assertEq(accountantHarness.getPendingRewards(vault, user), pendingRewards);
+    }
+
+    function test_ReturnsTheCorrectVaultPendingRewards(uint128 pendingRewards)
+        external
+        _cheat_replaceAccountantWithAccountantHarness
+    {
+        // it returns the correct vault pending rewards
+
+        AccountantHarness accountantHarness = AccountantHarness(address(accountant));
+
+        // We are putting the contract into a state where the vault has a non-null state
+        // This function is a testing-only function that shortcut the real end-user flow
+        accountantHarness._cheat_updateVaultData(
+            vault, Accountant.VaultData({integral: 0, supply: 0, feeSubjectAmount: 0, totalAmount: pendingRewards})
+        );
+
+        assertEq(accountantHarness.getPendingRewards(vault), pendingRewards);
+    }
+}
