7e63589a378ea0a63ba5e0f7d920ca65f0b41e38
qd-qd <qdqdqdqdqd@protonmail.com>
2025-03-05 17:29:26 +0100

    ✅ test: Accountant's getHarvestFeePercent method

---
+++ packages/strategies/test/unit/Accountant/getHarvestFeePercent.t.sol
@@ -0,0 +1,38 @@
+pragma solidity 0.8.28;
+
+import {BaseTest} from "test/Base.t.sol";
+import {stdStorage, StdStorage} from "forge-std/src/Test.sol";
+import {AccountantHarness} from "test/unit/Accountant/AccountantHarness.t.sol";
+
+contract Accountant__getHarvestFeePercent is BaseTest {
+    using stdStorage for StdStorage;
+
+    function test_ReturnsTheCorrectValue(uint128 newHarvestFeePercent)
+        external
+        _cheat_replaceAccountantWithAccountantHarness
+    {
+        // it returns the default protocol fee
+
+        AccountantHarness accountantHarness = AccountantHarness(address(accountant));
+
+        // test the function returns the default value in the contract
+        assertEq(accountantHarness.getHarvestFeePercent(), accountantHarness.getCurrentHarvestFee());
+
+        // illegally modify the value stored in the contract then test the getter returns it
+        accountantHarness._cheat_updateFeesParamsHarvestFeePercent(newHarvestFeePercent);
+        assertEq(accountantHarness.getHarvestFeePercent(), newHarvestFeePercent);
+    }
+
+    function test_ReadsTheStorage() external {
+        // it reads the storage
+
+        // record storage reads/writes
+        vm.record();
+        accountant.getHarvestFeePercent();
+        (bytes32[] memory reads, bytes32[] memory writes) = vm.accesses(address(accountant));
+
+        // ensure the value is read from the storage
+        assertEq(reads.length, 1);
+        assertEq(writes.length, 0);
+    }
+}
