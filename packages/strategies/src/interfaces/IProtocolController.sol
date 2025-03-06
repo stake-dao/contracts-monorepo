dac462fc8a26649c7d7fa3ece4bdae2954236bba
Warren <99427649+kobe-eth@users.noreply.github.com>
2025-03-06 12:53:06 +0100

    feat: Enhance Factory and ProtocolController with more granular vault and permission management

--- packages/strategies/src/interfaces/IProtocolController.sol
+++ packages/strategies/src/interfaces/IProtocolController.sol
@@ -7,6 +7,7 @@ interface IProtocolController {
     function rewardReceiver(address) external view returns (address);
 
     function allowed(address, address, bytes4 selector) external view returns (bool);
+    function permissionSetters(address) external view returns (bool);
 
     function strategy(bytes4 protocolId) external view returns (address);
     function allocator(bytes4 protocolId) external view returns (address);
@@ -17,4 +18,7 @@ interface IProtocolController {
 
     function registerVault(address _gauge, address _vault, address _asset, address _rewardReceiver, bytes4 _protocolId)
         external;
+
+    function setPermissionSetter(address _setter, bool _allowed) external;
+    function setPermission(address _contract, address _caller, bytes4 _selector, bool _allowed) external;
 }
