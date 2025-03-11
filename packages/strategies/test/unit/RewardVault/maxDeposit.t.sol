c4481829557ee2c070be3758e1b3be818a952307
qd-qd <qdqdqdqdqd@protonmail.com>
2025-03-11 15:47:38 +0100

    ✅ test(rewardvault): Test `maxDeposit`

--- packages/strategies/test/unit/RewardVault/maxDeposit.t.sol
+++ packages/strategies/test/unit/RewardVault/maxDeposit.t.sol
@@ -3,7 +3,9 @@ pragma solidity 0.8.28;
 import {RewardVaultBaseTest} from "test/RewardVaultBaseTest.sol";
 
 contract RewardVault__maxDeposit is RewardVaultBaseTest {
-    function test_ReturnsTheMaxUint256() external {
+    function test_ReturnsTheMaxUint256(address token) external {
         // it returns the max uint256
+
+        assertEq(rewardVault.maxDeposit(token), type(uint256).max);
     }
 }
