e367f554c37bf7e4ee6557c86a567c047cb44bce
qd-qd <qdqdqdqdqd@protonmail.com>
2025-03-11 15:45:08 +0100

    ✅ test(rewardvault): Test `allocator`

--- packages/strategies/test/unit/RewardVault/allocator.t.sol
+++ packages/strategies/test/unit/RewardVault/allocator.t.sol
@@ -2,8 +2,20 @@ pragma solidity 0.8.28;
 
 import {RewardVaultBaseTest} from "test/RewardVaultBaseTest.sol";
 
+import {RewardVaultBaseTest} from "test/RewardVaultBaseTest.sol";
+import {RewardVault} from "src/RewardVault.sol";
+import {IProtocolController} from "src/interfaces/IProtocolController.sol";
+
 contract RewardVault__allocator is RewardVaultBaseTest {
-    function test_ReturnsTheAllocatorAssociatedWithTheVault() external {
+    function test_ReturnsTheAllocatorAssociatedWithTheVault(address allocator) external {
         // it returns the allocator associated with the vault
+
+        vm.mockCall(
+            address(protocolController),
+            abi.encodeWithSelector(IProtocolController.allocator.selector),
+            abi.encode(allocator)
+        );
+
+        assertEq(address(rewardVault.allocator()), allocator);
     }
 }
