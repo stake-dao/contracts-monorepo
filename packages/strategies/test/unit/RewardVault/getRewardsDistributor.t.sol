c712307e295d112c7ed85f47b043d45d1f7af88e
qd-qd <qdqdqdqdqd@protonmail.com>
2025-03-11 15:46:55 +0100

    ✅ test(rewardvault): Test `getRewardDistributor`

--- packages/strategies/test/unit/RewardVault/getRewardsDistributor.t.sol
+++ packages/strategies/test/unit/RewardVault/getRewardsDistributor.t.sol
@@ -1,9 +1,29 @@
 pragma solidity 0.8.28;
 
-import {RewardVaultBaseTest} from "test/RewardVaultBaseTest.sol";
+import {RewardVaultBaseTest, RewardVaultHarness} from "test/RewardVaultBaseTest.sol";
+import {RewardVault} from "src/RewardVault.sol";
 
 contract RewardVault__getRewardsDistributor is RewardVaultBaseTest {
-    function test_ReturnsTheDistributorAddressForAGivenRewardToken() external {
-        // it returns the distributor address for a given reward token
+    function test_ReturnsTheDistributorAddressForAGivenRewardToken(address token, address distributor)
+        external
+        _cheat_replaceRewardVaultWithRewardVaultHarness
+    {
+        // it returns the reward for a given reward token
+
+        // create fake reward data
+        RewardVaultHarness rewardVaultHarness = RewardVaultHarness(address(rewardVault));
+
+        RewardVault.RewardData memory rewardData = RewardVault.RewardData({
+            rewardsDistributor: distributor,
+            rewardsDuration: 0,
+            lastUpdateTime: 0,
+            periodFinish: 0,
+            rewardRate: 0,
+            rewardPerTokenStored: 0
+        });
+
+        rewardVaultHarness._cheat_override_reward_data(token, rewardData);
+
+        assertEq(rewardVaultHarness.getRewardsDistributor(token), distributor);
     }
 }
