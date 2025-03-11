4bb211ccaaa06a58a82c4e54514d7fd6ed7c470d
qd-qd <qdqdqdqdqd@protonmail.com>
2025-03-11 15:47:44 +0100

    ✅ test(rewardvault): Test `maxMint`

--- packages/strategies/test/unit/RewardVault/maxMint.t.sol
+++ packages/strategies/test/unit/RewardVault/maxMint.t.sol
@@ -3,7 +3,9 @@ pragma solidity 0.8.28;
 import {RewardVaultBaseTest} from "test/RewardVaultBaseTest.sol";
 
 contract RewardVault__maxMint is RewardVaultBaseTest {
-    function test_ReturnsTheMaxUint256() external {
+    function test_ReturnsTheMaxUint256(address token) external {
         // it returns the max uint256
+
+        assertEq(rewardVault.maxMint(token), type(uint256).max);
     }
 }
