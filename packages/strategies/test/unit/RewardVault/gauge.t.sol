3bd3065da676077d54899104d62b33d26b67a057
qd-qd <qdqdqdqdqd@protonmail.com>
2025-03-11 15:46:04 +0100

    ✅ test(rewardvault): Test `gauge`

--- packages/strategies/test/unit/RewardVault/gauge.t.sol
+++ packages/strategies/test/unit/RewardVault/gauge.t.sol
@@ -1,9 +1,28 @@
 pragma solidity 0.8.28;
 
 import {RewardVaultBaseTest} from "test/RewardVaultBaseTest.sol";
+import {RewardVault} from "src/RewardVault.sol";
+import {BaseTest} from "test/Base.t.sol";
+import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
 
 contract RewardVault__gauge is RewardVaultBaseTest {
-    function test_ReturnsTheImmutableGaugeAddressPassedToTheClone() external {
+    function test_ReturnsTheImmutableGaugeAddressPassedToTheClone(address gauge) external {
         // it returns the immutable gauge address passed to the clone
+
+        CloneFactoryRewardVaultTest cloneFactory = new CloneFactoryRewardVaultTest();
+        address cloneRewardVault = cloneFactory.deployClone(address(rewardVault), gauge, makeAddr("asset"));
+
+        assertEq(RewardVault(cloneRewardVault).gauge(), gauge);
+    }
+}
+
+contract CloneFactoryRewardVaultTest is BaseTest {
+    function deployClone(address implementation, address gauge, address asset) external returns (address) {
+        // The asset() function reads from offset 80, so we need to ensure asset is at that position
+        // We use abi.encodePacked to ensure proper byte alignment
+        // TODO: What's the purpose of the buffer?
+        bytes memory encodedData = abi.encodePacked(makeAddr("buffer"), makeAddr("buffer"), gauge, asset);
+
+        return Clones.cloneWithImmutableArgs(implementation, encodedData);
     }
 }
