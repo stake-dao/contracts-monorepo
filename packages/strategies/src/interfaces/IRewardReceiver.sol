3b76db4b5029773f2c3a12e3ab4e2245398f3a6a
qd-qd <qdqdqdqdqd@protonmail.com>
2025-03-14 10:16:00 +0100

    ♻️✅ test(rewardvault): RewardReceiver tests
    This commit slighty refacto the RewardReceiver contract and implements all the unit tests for the contrat

---
+++ packages/strategies/src/interfaces/IRewardReceiver.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.28;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+interface IRewardReceiver {
+    function distributeRewards() external;
+    function distributeRewardToken(IERC20 token) external;
+}
