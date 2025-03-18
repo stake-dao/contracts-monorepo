40a97e3881f97b24ea587d3dc3d9be79d2d395d7
Warren <99427649+kobe-eth@users.noreply.github.com>
2025-03-18 14:22:52 +0100

    fix: improve test

--- packages/strategies/test/integration/curve/ConvexPools.t.sol
+++ packages/strategies/test/integration/curve/ConvexPools.t.sol
@@ -5,136 +5,49 @@ import "test/integration/curve/CurveIntegration.t.sol";
 import "test/integration/curve/CurveFactory.t.sol";
 
 // @notice Selected Convex pool PIDs sorted by PID (highest to lowest)
+// @dev Each integration test requires two PIDs to test multi-gauge functionality
+// @dev Make sure both gauges are properly initialized with _setupGauge in setUp
 
 uint256 constant CONVEX_POOL_437_PID = 437;
-/// Convex Pool PID 437 with gauge 0x1C94126F29A9d6F9Ef3CCB1D5D58EbcaCC519F85
-
-contract _CONVEX_POOL_437_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_437_PID) {}
-
-contract _CONVEX_POOL_437_PID_Deposit_Test is CurveIntegrationTest(CONVEX_POOL_437_PID) {}
 
 uint256 constant CONVEX_POOL_436_PID = 436;
-/// Convex Pool PID 436 with gauge 0x411387Be45F5c001159f3d76e26Fe7D200e5Dc71
-
-contract _CONVEX_POOL_436_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_436_PID) {}
-
-contract _CONVEX_POOL_436_PID_Deposit_Test is CurveIntegrationTest(CONVEX_POOL_436_PID) {}
 
 uint256 constant CONVEX_POOL_435_PID = 435;
-/// Convex Pool PID 435 with gauge 0x63f30b3f61e9F123a0051B183BB7489EB537E131
-
-contract _CONVEX_POOL_435_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_435_PID) {}
-
-contract _CONVEX_POOL_435_PID_Deposit_Test is CurveIntegrationTest(CONVEX_POOL_435_PID) {}
 
 uint256 constant CONVEX_POOL_434_PID = 434;
-/// Convex Pool PID 434 with gauge 0x3D59052DEa0e0ffd9b8A59A6faE13ec037850155
-
-contract _CONVEX_POOL_434_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_434_PID) {}
-
-contract _CONVEX_POOL_434_PID_Deposit_Test is CurveIntegrationTest(CONVEX_POOL_434_PID) {}
 
 uint256 constant CONVEX_POOL_433_PID = 433;
-/// Convex Pool PID 433 with gauge 0xDDCFb091f3FdCaDEe38465Da9A73cb5e356E145b
-
-contract _CONVEX_POOL_433_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_433_PID) {}
-
-contract _CONVEX_POOL_433_PID_Deposit_Test is CurveIntegrationTest(CONVEX_POOL_433_PID) {}
 
 uint256 constant CONVEX_POOL_432_PID = 432;
-/// Convex Pool PID 432 with gauge 0x326b6d1a26Fd04afAa349340022076b5a2eb0c7C
-
-contract _CONVEX_POOL_432_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_432_PID) {}
-
-contract _CONVEX_POOL_432_PID_Deposit_Test is CurveIntegrationTest(CONVEX_POOL_432_PID) {}
 
 uint256 constant CONVEX_POOL_431_PID = 431;
-/// Convex Pool PID 431 with gauge 0xbD7ddCe4D5a97F102DE674Ef56823b600e843C8A
-
-contract _CONVEX_POOL_431_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_431_PID) {}
-
-contract _CONVEX_POOL_431_PID_Deposit_Test is CurveIntegrationTest(CONVEX_POOL_431_PID) {}
 
 uint256 constant CONVEX_POOL_430_PID = 430;
-/// Convex Pool PID 430 with gauge 0x52618C40dDBA3cBbb69F3aAA4CB26Ae649844B17
 
-contract _CONVEX_POOL_430_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_430_PID) {}
+contract CONVEX_POOL_437_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_437_PID) {}
 
-contract _CONVEX_POOL_430_PID_Deposit_Test is CurveIntegrationTest(CONVEX_POOL_430_PID) {}
+// Integration test using 437 and 436
+contract CONVEX_POOL_437_PID_Integration_Test is CurveIntegrationTest(CONVEX_POOL_437_PID, CONVEX_POOL_436_PID) {}
 
-uint256 constant CONVEX_POOL_429_PID = 429;
-/// Convex Pool PID 429 with gauge 0x5Dd3e384621e00a9fe1868c257c03FA78AE24e47
+contract CONVEX_POOL_436_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_436_PID) {}
 
-contract _CONVEX_POOL_429_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_429_PID) {}
+contract CONVEX_POOL_435_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_435_PID) {}
 
-contract _CONVEX_POOL_429_PID_Deposit_Test is CurveIntegrationTest(CONVEX_POOL_429_PID) {}
+// Integration test using 435 and 434
+contract CONVEX_POOL_435_PID_Integration_Test is CurveIntegrationTest(CONVEX_POOL_435_PID, CONVEX_POOL_434_PID) {}
 
-uint256 constant CONVEX_POOL_428_PID = 428;
-/// Convex Pool PID 428 with gauge 0x74231E4d96498A30FCEaf9aACCAbBD79339Ecd7f
+contract CONVEX_POOL_434_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_434_PID) {}
 
-contract _CONVEX_POOL_428_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_428_PID) {}
+contract CONVEX_POOL_433_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_433_PID) {}
 
-contract _CONVEX_POOL_428_PID_Deposit_Test is CurveIntegrationTest(CONVEX_POOL_428_PID) {}
+// Integration test using 433 and 432
+contract CONVEX_POOL_433_PID_Integration_Test is CurveIntegrationTest(CONVEX_POOL_433_PID, CONVEX_POOL_432_PID) {}
 
-uint256 constant CONVEX_POOL_427_PID = 427;
-/// Convex Pool PID 427 with gauge 0x0d2d1e35D1852A90BCeDf0B119eFb4885595F374
+contract CONVEX_POOL_432_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_432_PID) {}
 
-contract _CONVEX_POOL_427_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_427_PID) {}
+contract CONVEX_POOL_431_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_431_PID) {}
 
-contract _CONVEX_POOL_427_PID_Deposit_Test is CurveIntegrationTest(CONVEX_POOL_427_PID) {}
+// Integration test using 431 and 430
+contract CONVEX_POOL_431_PID_Integration_Test is CurveIntegrationTest(CONVEX_POOL_431_PID, CONVEX_POOL_430_PID) {}
 
-uint256 constant CONVEX_POOL_426_PID = 426;
-/// Convex Pool PID 426 with gauge 0x9f330Db38caAAe5B61B410e2f0aaD63fff2109d8
-
-contract _CONVEX_POOL_426_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_426_PID) {}
-
-contract _CONVEX_POOL_426_PID_Deposit_Test is CurveIntegrationTest(CONVEX_POOL_426_PID) {}
-
-uint256 constant CONVEX_POOL_425_PID = 425;
-/// Convex Pool PID 425 with gauge 0x479dFB03cdDEa20dC4e8788B81Fd7C7A08FD3555
-
-contract _CONVEX_POOL_425_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_425_PID) {}
-
-contract _CONVEX_POOL_425_PID_Deposit_Test is CurveIntegrationTest(CONVEX_POOL_425_PID) {}
-
-uint256 constant CONVEX_POOL_424_PID = 424;
-/// Convex Pool PID 424 with gauge 0x8B859fb47b6377a84B61D3891774De462560742C
-
-contract _CONVEX_POOL_424_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_424_PID) {}
-
-contract _CONVEX_POOL_424_PID_Deposit_Test is CurveIntegrationTest(CONVEX_POOL_424_PID) {}
-
-uint256 constant CONVEX_POOL_423_PID = 423;
-/// Convex Pool PID 423 with gauge 0xA8bB51606dB8e81F0ac964f0855C20D9e474Ab63
-
-contract _CONVEX_POOL_423_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_423_PID) {}
-
-contract _CONVEX_POOL_423_PID_Deposit_Test is CurveIntegrationTest(CONVEX_POOL_423_PID) {}
-
-uint256 constant CONVEX_POOL_422_PID = 422;
-/// Convex Pool PID 422 with gauge 0xc372CCdD24Bb0753CEB78bBaC4D24EB7dE4aBE4e
-
-contract _CONVEX_POOL_422_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_422_PID) {}
-
-contract _CONVEX_POOL_422_PID_Deposit_Test is CurveIntegrationTest(CONVEX_POOL_422_PID) {}
-
-uint256 constant CONVEX_POOL_421_PID = 421;
-/// Convex Pool PID 421 with gauge 0xd303994a0Db9b74f3E8fF629ba3097fC7060C331
-
-contract _CONVEX_POOL_421_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_421_PID) {}
-
-contract _CONVEX_POOL_421_PID_Deposit_Test is CurveIntegrationTest(CONVEX_POOL_421_PID) {}
-
-uint256 constant CONVEX_POOL_420_PID = 420;
-/// Convex Pool PID 420 with gauge 0x6eb6B5915432c890dF1999a491A6929998634bAb
-
-contract _CONVEX_POOL_420_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_420_PID) {}
-
-contract _CONVEX_POOL_420_PID_Deposit_Test is CurveIntegrationTest(CONVEX_POOL_420_PID) {}
-
-uint256 constant CONVEX_POOL_419_PID = 419;
-/// Convex Pool PID 419 with gauge 0xa48A3c91b062ca06Fd0d0569695432EB066f8c7E
-
-contract _CONVEX_POOL_419_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_419_PID) {}
-
-contract _CONVEX_POOL_419_PID_Deposit_Test is CurveIntegrationTest(CONVEX_POOL_419_PID) {}
+contract CONVEX_POOL_430_PID_Factory_Test is CurveFactoryTest(CONVEX_POOL_430_PID) {}
