eb13b1ef3693ea8969b0f84fc3fa80cce9b69d82
qd-qd <qdqdqdqdqd@protonmail.com>
2025-03-20 14:49:21 +0100

    test(lockers): Test `PreLaunchLocker.getUserFriendlyStateLabel`

--- packages/lockers/test/PreLaunchLocker/getUserFriendlyStateLabel.t.sol
+++ packages/lockers/test/PreLaunchLocker/getUserFriendlyStateLabel.t.sol
@@ -1,14 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 
-import {Test} from "forge-std/src/Test.sol";
+import {PreLaunchLockerTest} from "test/PreLaunchLocker/utils/PreLaunchLockerTest.t.sol";
 import {PreLaunchLocker} from "src/common/locker/PreLaunchLocker.sol";
 
-contract PreLaunchLocker__getUserFriendlyStateLabel is Test {
+contract PreLaunchLocker__getUserFriendlyStateLabel is PreLaunchLockerTest {
     function test_ReturnsDifferentValuesBasedOnTheState() external {
         // it returns different values based on the state
-
-        PreLaunchLocker locker = new PreLaunchLocker(makeAddr("token"));
 
         string memory idleLabel = locker.getUserFriendlyStateLabel(PreLaunchLocker.STATE.IDLE);
         string memory activeLabel = locker.getUserFriendlyStateLabel(PreLaunchLocker.STATE.ACTIVE);
