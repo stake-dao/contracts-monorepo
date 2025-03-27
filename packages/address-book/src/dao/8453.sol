109df9f7f556bbada887873c89cd2308e4e4b409
LucasRamadour <72853747+LucasRamadour@users.noreply.github.com>
2025-03-27 12:49:53 +0100

    feat(lockers/spectra): ✨ Add Spectra deploy script

---
+++ packages/address-book/src/dao/8453.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.6.0;
+
+library DAO {
+    address public constant MAIN_DEPLOYER = 0x000755Fbe4A24d7478bfcFC1E561AfCE82d1ff62;
+    address public constant TREASURY = 0x5DA07af8913A4EAf09E5F569c20138b658906c17;
+    address public constant GOVERNANCE = 0xB0552b6860CE5C0202976Db056b5e3Cc4f9CC765;
+}
