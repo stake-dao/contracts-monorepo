25a34774f6b974caeac357b65cdbd4559db2ff42
qd-qd <qdqdqdqdqd@protonmail.com>
2025-04-10 16:35:05 +0200

    test(strategies): RouterModuleDeposit tests
    Test `getters` and `deposit`

---
+++ packages/strategies/test/unit/Router/RouterModules/RouterModuleDeposit/getters.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+import {Test} from "forge-std/src/Test.sol";
+import {RouterModuleDeposit} from "src/RouterModules/RouterModuleDeposit.sol";
+
+contract RouterModuleDeposit__getters is Test {
+    RouterModuleDeposit internal module;
+
+    function setUp() public {
+        module = new RouterModuleDeposit();
+    }
+
+    function test_ReturnsTheName() external view {
+        // it returns the name of the module
+        assertEq(module.name(), type(RouterModuleDeposit).name);
+    }
+
+    function test_ReturnsTheVersion() external view {
+        // it returns the version
+        assertEq(module.version(), "1.0.0");
+    }
+}
