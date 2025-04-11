7d2469ff12296591e068db8089595ff86bfe0adf
qd-qd <qdqdqdqdqd@protonmail.com>
2025-04-11 10:56:17 +0200

    test(strategies): `Router` scripts tests

---
+++ packages/strategies/test/unit/Scripts/Router/04_SetupRouterWithdrawModule.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+import {Test} from "forge-std/src/Test.sol";
+import {DeployAndSetWithdrawModule} from "script/Router/04_DeployAndSetWithdrawModule.s.sol";
+import {Router} from "src/Router.sol";
+import {RouterIdentifierMapping} from "src/RouterModules/RouterIdentifierMapping.sol";
+import {RouterModuleWithdraw} from "src/RouterModules/RouterModuleWithdraw.sol";
+
+contract Router__SetupRouterWithdrawModuleScript is Test, DeployAndSetWithdrawModule {
+    function test_RouterWithdrawModuleIsCorrectlyDeployed(bytes32 salt) external {
+        Router router = new Router{salt: salt}();
+
+        _run(address(router));
+
+        assertEq(router.getModuleName(RouterIdentifierMapping.WITHDRAW), type(RouterModuleWithdraw).name);
+    }
+}
