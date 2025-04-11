88321c879495c0e16cc4a0a2884dbd4d2a9fe3ea
qd-qd <qdqdqdqdqd@protonmail.com>
2025-04-11 10:55:54 +0200

    feat(strategies): `Router` scripts
    - Added scripts to deploy the Router contract and set up its modules: Deposit, Withdraw, and Claim.
    - Introduced RouterIdentifierMapping for module identification. This library serves as a suggestion rather than a strict rules to follow
    - Each module has a dedicated script for deployment and configuration.
    - One of the script set up all of the developed modules

---
+++ packages/strategies/script/Router/02_SetupRouterModules.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.28;
+
+import {Script} from "forge-std/src/Script.sol";
+import {Router} from "src/Router.sol";
+import {RouterIdentifierMapping} from "src/RouterModules/RouterIdentifierMapping.sol";
+import {RouterModuleClaim} from "src/RouterModules/RouterModuleClaim.sol";
+import {RouterModuleDeposit} from "src/RouterModules/RouterModuleDeposit.sol";
+import {RouterModuleWithdraw} from "src/RouterModules/RouterModuleWithdraw.sol";
+
+contract SetRouterModules is Script {
+    function _run(address router) internal {
+        Router(router).setModule(RouterIdentifierMapping.DEPOSIT, address(new RouterModuleDeposit()));
+        Router(router).setModule(RouterIdentifierMapping.WITHDRAW, address(new RouterModuleWithdraw()));
+        Router(router).setModule(RouterIdentifierMapping.CLAIM, address(new RouterModuleClaim()));
+    }
+
+    function run() external {
+        vm.startBroadcast();
+
+        _run(vm.envAddress("ROUTER"));
+
+        vm.stopBroadcast();
+    }
+}
---
+++ packages/strategies/src/RouterModules/RouterIdentifierMapping.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.28;
+
+library RouterIdentifierMapping {
+    uint8 internal constant DEPOSIT = 0x00;
+    uint8 internal constant WITHDRAW = 0x01;
+    uint8 internal constant CLAIM = 0x02;
+}
