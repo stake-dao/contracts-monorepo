75360b64d393bf0a54d098152bb63c533f0f8438
qd-qd <qdqdqdqdqd@protonmail.com>
2025-04-14 17:59:33 +0200

    test(strategies): `RouterModuleMigrationCurve` tests

---
+++ packages/strategies/test/unit/Router/RouterModules/RouterModuleMigrationCurve/getters.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+import {Test} from "forge-std/src/Test.sol";
+import {RouterModuleMigrationCurve} from "src/RouterModules/RouterModuleMigrationCurve.sol";
+
+contract RouterModuleMigrationCurve__getters is Test {
+    RouterModuleMigrationCurve internal module;
+
+    function setUp() public {
+        module = new RouterModuleMigrationCurve();
+    }
+
+    function test_ReturnsTheName() external view {
+        // it returns the name of the module
+        assertEq(module.name(), type(RouterModuleMigrationCurve).name);
+    }
+
+    function test_ReturnsTheVersion() external view {
+        // it returns the version
+        assertEq(module.version(), "1.0.0");
+    }
+}
