27c0f911bfd0c027a1509b584059642c7057e3db
qd-qd <qdqdqdqdqd@protonmail.com>
2025-04-14 17:13:36 +0200

    test(strategies): `RouterModuleMigrationStakeDAOV1` scripts

--- packages/strategies/script/Router/06_DeployAndSetMigrationStakeDaoModule.s.sol
+++ packages/strategies/script/Router/06_DeployAndSetMigrationStakeDaoModule.s.sol
@@ -4,25 +4,22 @@ pragma solidity 0.8.28;
 import {Script} from "forge-std/src/Script.sol";
 import {Router} from "src/Router.sol";
 import {RouterIdentifierMapping} from "src/RouterModules/RouterIdentifierMapping.sol";
-import {RouterModuleClaim} from "src/RouterModules/RouterModuleClaim.sol";
-import {RouterModuleDeposit} from "src/RouterModules/RouterModuleDeposit.sol";
 import {RouterModuleMigrationStakeDAOV1} from "src/RouterModules/RouterModuleMigrationStakeDAOV1.sol";
-import {RouterModuleWithdraw} from "src/RouterModules/RouterModuleWithdraw.sol";
 
-contract SetRouterModules is Script {
+contract DeployAndSetMigrationStakeDaoModule is Script {
     function _run(address router) internal {
-        Router(router).setModule(RouterIdentifierMapping.DEPOSIT, address(new RouterModuleDeposit()));
-        Router(router).setModule(RouterIdentifierMapping.WITHDRAW, address(new RouterModuleWithdraw()));
-        Router(router).setModule(RouterIdentifierMapping.CLAIM, address(new RouterModuleClaim()));
         Router(router).setModule(
             RouterIdentifierMapping.MIGRATION_STAKE_DAO_V1, address(new RouterModuleMigrationStakeDAOV1())
         );
     }
 
+    /// @notice Deploy the RouterModuleMigrationStakeDAOV1 contract and set it in the Router
+    /// @dev The `ROUTER` (address) environment variable must be set
     function run() external {
         vm.startBroadcast();
 
-        _run(vm.envAddress("ROUTER"));
+        address router = vm.envAddress("ROUTER");
+        _run(router);
 
         vm.stopBroadcast();
     }
--- packages/strategies/src/RouterModules/RouterIdentifierMapping.sol
+++ packages/strategies/src/RouterModules/RouterIdentifierMapping.sol
@@ -5,4 +5,5 @@ library RouterIdentifierMapping {
     uint8 internal constant DEPOSIT = 0x00;
     uint8 internal constant WITHDRAW = 0x01;
     uint8 internal constant CLAIM = 0x02;
+    uint8 internal constant MIGRATION_STAKE_DAO_V1 = 0x03;
 }
