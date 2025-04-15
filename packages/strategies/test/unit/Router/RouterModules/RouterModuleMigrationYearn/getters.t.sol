6d38aeeb30c5134a2ada47f14800991c1bd59419
qd-qd <qdqdqdqdqd@protonmail.com>
2025-04-15 18:48:53 +0200

    test(strategies): `RouterModuleMigrationYearn` tests

---
+++ packages/strategies/test/unit/Router/RouterModules/RouterModuleMigrationYearn/getters.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+import {Test} from "forge-std/src/Test.sol";
+import {RouterModuleMigrationYearn} from "src/RouterModules/RouterModuleMigrationYearn.sol";
+
+contract RouterModuleMigrationYearn__getters is Test {
+    RouterModuleMigrationYearn internal module;
+
+    function setUp() public {
+        module = new RouterModuleMigrationYearn();
+    }
+
+    function test_ReturnsTheName() external view {
+        // it returns the name of the module
+        assertEq(module.name(), type(RouterModuleMigrationYearn).name);
+    }
+
+    function test_ReturnsTheVersion() external view {
+        // it returns the version
+        assertEq(module.version(), "1.0.0");
+    }
+}
