96e5fde4d0f76e14102e6b4e5b46a5cfa2af909e
Marsot Pierre <pierre.mmarsot@gmail.com>
2025-04-15 14:48:01 +0200

    new spectra claimer

--- packages/safe-modules/src/interfaces/ISpectraVoter.sol
+++ packages/safe-modules/src/interfaces/ISpectraVoter.sol
@@ -5,4 +5,5 @@ interface ISpectraVoter {
     function length() external view returns(uint256);
     function poolIds(uint256 poolId) external view returns(uint160);
     function poolToBribe(uint160 poolId) external view returns(address);
+    function poolToFees(uint160 poolId) external view returns(address);
 }
