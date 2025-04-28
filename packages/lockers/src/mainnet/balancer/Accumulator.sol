da73b78c297ea02ee2ff2b7fdaf07e547cc689bb
qd-qd <qdqdqdqdqd@protonmail.com>
2025-04-28 23:26:30 +0200

    feat(lockers): Balancer locker v4

--- packages/lockers/src/mainnet/balancer/Accumulator.sol
+++ packages/lockers/src/mainnet/balancer/Accumulator.sol
@@ -1,48 +1,81 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.19;
 
-import "src/common/accumulator/BaseAccumulator.sol";
-import {ILocker} from "src/common/interfaces/ILocker.sol";
-import "src/common/interfaces/IVeBoost.sol";
-import "src/common/interfaces/IVeBoostDelegation.sol";
+import {BaseAccumulator} from "src/common/accumulator/BaseAccumulator.sol";
+import {SafeModule} from "src/common/utils/SafeModule.sol";
+import {IVeBoost} from "src/common/interfaces/IVeBoost.sol";
+import {IVeBoostDelegation} from "src/common/interfaces/IVeBoostDelegation.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {SafeTransferLib} from "solady/src/utils/SafeTransferLib.sol";
+import {IFeeReceiver} from "common/interfaces/IFeeReceiver.sol";
+import {ILiquidityGauge} from "src/common/interfaces/ILiquidityGauge.sol";
+import {Balancer} from "address-book/src/protocols/1.sol";
+import {BalancerFeeDistributor} from "src/common/interfaces/BalancerFeeDistributor.sol";
+import {BAL as BalancerProtocol} from "address-book/src/lockers/1.sol";
 
 /// @notice BAL BaseAccumulator
 /// @author StakeDAO
-contract Accumulator is BaseAccumulator {
+contract BalancerAccumulator is BaseAccumulator, SafeModule {
+    ///////////////////////////////////////////////////////////////
+    /// --- CONSTANTS
+    ///////////////////////////////////////////////////////////////
+
     /// @notice BAL token address.
-    address public constant BAL = 0xba100000625a3754423978a60c9317c58a424e3D;
+    address public constant BAL = Balancer.BAL;
 
     /// @notice USDC token address.
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
     /// @notice VeBAL token address.
-    address public constant VE_BAL = 0xC128a9954e6c874eA3d62ce62B468bA073093F25;
+    address public constant VE_BAL = Balancer.VEBAL;
 
-    /// @notice Ve Boost.
+    /// @notice Fee distributor address.
+    address public constant FEE_DISTRIBUTOR = 0xD3cf852898b21fc233251427c2DC93d3d604F3BB;
+
+    ///////////////////////////////////////////////////////////////
+    /// --- STATES
+    ///////////////////////////////////////////////////////////////
+
+    /// @notice Ve Boost V2
     IVeBoost public veBoost = IVeBoost(0x67F8DF125B796B05895a6dc8Ecf944b9556ecb0B);
 
     /// @notice Ve Boost FXTLDelegation.
     IVeBoostDelegation public veBoostDelegation = IVeBoostDelegation(0xda9846665Bdb44b0d0CAFFd0d1D4A539932BeBdf);
 
+    /// @notice Multiplier applied to the delegation share of BAL rewards sent to veBoost delegators.
+    /// @dev Scales the calculated delegation share. Set as a fixed-point value with 1e18 = 100%.
     uint256 public multiplier;
 
-    constructor(address _gauge, address _locker, address _governance)
+    ////////////////////////////////////////////////////////////
+    /// --- CONSTRUCTOR
+    ////////////////////////////////////////////////////////////
+
+    constructor(address _gauge, address _locker, address _governance, address _gateway)
         BaseAccumulator(_gauge, USDC, _locker, _governance)
+        SafeModule(_gateway)
     {
+        strategy = BalancerProtocol.STRATEGY;
+        // Give full approval to the gauge for the BAL and USDC tokens
         SafeTransferLib.safeApprove(BAL, _gauge, type(uint256).max);
         SafeTransferLib.safeApprove(USDC, _gauge, type(uint256).max);
     }
 
-    function claimAndNotifyAll(bool notifySDT, bool claimFeeStrategy) external override {
-        ILocker(locker).claimRewards(USDC, address(this));
+    ///////////////////////////////////////////////////////////////
+    /// --- OVERRIDDEN FUNCTIONS
+    ///////////////////////////////////////////////////////////////
 
-        // Claim Extra USDC rewards.
-        if (claimFeeStrategy) {
-            _claimFeeStrategy();
-        }
+    /// @notice Make the locker claim all the reward tokens before depositing them to the Liquidity Gauge (v4)
+    function claimAndNotifyAll() external override {
+        // Claim locker's claimable USDC rewards and transfer them here
+        uint256 claimed = _execute_claimRewards();
+        _execute_transfer(claimed);
 
+        // Tell the Strategy to send the accrued fees to the fee receiver
+        _claimFeeStrategy();
+
+        // Notify the rewards to the Liquidity Gauge (V4)
         notifyReward(USDC, false, false);
-        notifyReward(BAL, notifySDT, claimFeeStrategy);
+        notifyReward(BAL, true, true);
     }
 
     function _notifyReward(address _tokenReward, uint256 _amount, bool _pullFromFeeReceiver) internal override {
@@ -92,10 +125,24 @@ contract Accumulator is BaseAccumulator {
         SafeTransferLib.safeTransfer(BAL, address(veBoostDelegation), delegationShare);
     }
 
-    /// @dev To be compatible with the "old" strategy.
-    function depositToken(address token, uint256 amount) external {
-        SafeTransferLib.safeTransferFrom(token, msg.sender, address(this), amount);
+    ////////////////////////////////////////////////////////////////
+    /// --- SAFE MODULE RELATED FUNCTIONS
+    ///////////////////////////////////////////////////////////////
+
+    function _execute_claimRewards() internal returns (uint256 claimed) {
+        bytes memory returnData = _executeTransaction(
+            FEE_DISTRIBUTOR, abi.encodeWithSelector(BalancerFeeDistributor.claimToken.selector, locker, USDC)
+        );
+        claimed = abi.decode(returnData, (uint256));
     }
+
+    function _execute_transfer(uint256 amount) internal {
+        _executeTransaction(USDC, abi.encodeWithSelector(ERC20.transfer.selector, address(this), amount));
+    }
+
+    ///////////////////////////////////////////////////////////////
+    /// --- SETTERS
+    ///////////////////////////////////////////////////////////////
 
     function setMultiplier(uint256 _multiplier) external onlyGovernance {
         multiplier = _multiplier;
@@ -109,7 +156,19 @@ contract Accumulator is BaseAccumulator {
         veBoostDelegation = IVeBoostDelegation(_veBoostDelegation);
     }
 
-    function name() external pure override returns (string memory) {
-        return "BAL Accumulator";
+    ///////////////////////////////////////////////////////////////
+    /// --- GETTERS
+    ///////////////////////////////////////////////////////////////
+
+    function _getLocker() internal view override returns (address) {
+        return locker;
+    }
+
+    function version() external pure virtual override returns (string memory) {
+        return "4.0.0";
+    }
+
+    function name() external view virtual override returns (string memory) {
+        return type(BalancerAccumulator).name;
     }
 }
