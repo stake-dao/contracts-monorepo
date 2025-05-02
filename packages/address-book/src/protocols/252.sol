2850fef76bf092222d7393b26b74715103727a9a
qd-qd <qdqdqdqdqd@protonmail.com>
2025-05-02 15:21:43 +0200

    feat(lockers): FXS locker v4 on Fraxtal

--- packages/address-book/src/protocols/252.sol
+++ packages/address-book/src/protocols/252.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.6.0;
 library Frax {
     address public constant FXS = 0xFc00000000000000000000000000000000000002;
     address public constant VEFXS = 0x007FD070a7E1B0fA1364044a373Ac1339bAD89CF;
-    address public constant YIELD_DISTRIBUTOR = 0x39333a540bbea6262e405E1A6d435Bd2e776561E;
+    address public constant YIELD_DISTRIBUTOR = 0x21359d1697e610e25C8229B2C57907378eD09A2E;
     address public constant DELEGATION_REGISTRY = 0xF5cA906f05cafa944c27c6881bed3DFd3a785b6A;
     address public constant FRAXTAL_BRIDGE = 0x4200000000000000000000000000000000000010;
 
--- packages/lockers/src/fraxtal/FXTLDelegation.sol
+++ packages/lockers/src/fraxtal/FXTLDelegation.sol
@@ -5,14 +5,17 @@ pragma solidity 0.8.19;
 /// @notice Helper to delegate FXTL Points.
 /// @author StakeDAO
 contract FXTLDelegation {
+    /// @notice Throwed on FXTL delegation failure
+    error FXTLDelegationFailed();
+
     constructor(address _delegationRegistry, address _initialDelegate) {
         /// Set initial delegate.
         (bool success,) =
             _delegationRegistry.call(abi.encodeWithSignature("setDelegationForSelf(address)", _initialDelegate));
-        if (!success) revert("CallFailed");
+        if (!success) revert FXTLDelegationFailed();
 
         /// Disable self managing delegation.
         (success,) = _delegationRegistry.call(abi.encodeWithSignature("disableSelfManagingDelegations()"));
-        if (!success) revert("CallFailed");
+        if (!success) revert FXTLDelegationFailed();
     }
 }
