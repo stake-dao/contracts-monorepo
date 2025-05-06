12ae65ac8aa33c1fc23fb6837fdf02497e7fd2af
qd-qd <qdqdqdqdqd@protonmail.com>
2025-05-06 18:21:04 +0200

    test(lockers): DrippingAccumulator unit tests

---
+++ packages/lockers/test/unit/DrippingAccumulator/utils/DrippingAccumulatorHarness.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.19 <0.9.0;
+
+import {DrippingAccumulator} from "src/common/accumulator/DrippingAccumulator.sol";
+
+contract DrippingAccumulatorHarness is DrippingAccumulator {
+    constructor(address _gauge, address _rewardToken, address _locker, address _governance, uint256 _periodLength)
+        DrippingAccumulator(_gauge, _rewardToken, _locker, _governance, _periodLength)
+    {}
+
+    function _expose_startNewDistribution() external {
+        startNewDistribution();
+    }
+
+    function _expose_advanceDistributionStep() external {
+        advanceDistributionStep();
+    }
+
+    function _expose_calculateDistributableReward() external view returns (uint256) {
+        return calculateDistributableReward();
+    }
+
+    function _expose_getCurrentWeekTimestamp() external view returns (uint256) {
+        return getCurrentWeekTimestamp();
+    }
+
+    function _expose_getCurrentRewardTokenBalance() external view returns (uint256) {
+        return getCurrentRewardTokenBalance();
+    }
+}
