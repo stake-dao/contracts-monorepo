1495d12c2f7e10de888e0d5adc4dd2774163e8f3
qd-qd <qdqdqdqdqd@protonmail.com>
2025-05-07 18:25:59 +0200

    test(lockers): DelegableAccumulator unit tests

---
+++ packages/lockers/test/unit/DelegableAccumulator/setVeBoost.t.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.19 <0.9.0;
+
+import {DelegableAccumulatorTest} from "test/unit/DelegableAccumulator/utils/DelegableAccumulatorTest.t.sol";
+import {BaseAccumulator} from "src/common/accumulator/BaseAccumulator.sol";
+
+contract DelegableAccumulator__setVeBoost is DelegableAccumulatorTest {
+    function test_RevertsIfTheCallerIsNotTheGovernance(address caller) external {
+        // it reverts if the caller is not the governance
+
+        vm.assume(caller != governance);
+
+        vm.prank(caller);
+        vm.expectRevert(BaseAccumulator.GOVERNANCE.selector);
+        delegableAccumulator.setVeBoost(makeAddr("veBoost"));
+    }
+
+    function test_SetsTheVeBoost(address _veBoost) external {
+        // it sets the veBoost
+
+        vm.assume(_veBoost != address(delegableAccumulator.veBoost()));
+
+        vm.prank(governance);
+        delegableAccumulator.setVeBoost(_veBoost);
+
+        assertEq(delegableAccumulator.veBoost(), _veBoost);
+    }
+
+    function test_EmitsAnEvent(address _veBoost) external {
+        // it emits an event
+
+        vm.expectEmit();
+        emit VeBoostSet(delegableAccumulator.veBoost(), _veBoost);
+
+        vm.prank(governance);
+        delegableAccumulator.setVeBoost(_veBoost);
+    }
+
+    /// @notice Event emitted when the veBoost is set
+    event VeBoostSet(address oldVeBoost, address newVeBoost);
+}
