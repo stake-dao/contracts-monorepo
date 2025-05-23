c11a2869580ef93096fd08b41d32600a90364fb0
qd-qd <qdqdqdqdqd@protonmail.com>
2025-05-23 17:48:06 +0200

    chore(lockers): align solc version

--- packages/lockers/test/fork/yieldnest/PreLaunchLocker.t.sol
+++ packages/lockers/test/fork/yieldnest/PreLaunchLocker.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.19;
+pragma solidity 0.8.28;
 
 import {Test} from "forge-std/src/Test.sol";
 import {YieldnestProtocol} from "address-book/src/YieldnestEthereum.sol";
