ae69ca73f5028783aa910177b19aa3aa82d9678f
qd-qd <qdqdqdqdqd@protonmail.com>
2025-06-13 16:43:08 +0200

    chore(book): add asdPENDLE
    - Add the asdPENDLE address to the address book
    - Create the IASDPendle interface in the lockers package

---
+++ packages/lockers/src/interfaces/IASDPendle.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import {ISdPendleCompounder} from "@aladdin/interfaces/concentrator/ISdPendleCompounder.sol";
+
+interface IASDPendle is ISdPendleCompounder {}
