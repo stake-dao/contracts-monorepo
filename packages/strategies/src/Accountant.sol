a20b86028c741bb8d653b7646c581b4422956578
Warren <99427649+kobe-eth@users.noreply.github.com>
2025-06-19 11:04:14 +0200

    refactor: simplify event parameters and remove unused imports in Accountant and ProtocolController

--- packages/strategies/src/Accountant.sol
+++ packages/strategies/src/Accountant.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.28;
 
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
-import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -24,7 +23,6 @@ contract Accountant is ReentrancyGuardTransient, Ownable2Step, IAccountant {
     using Math for uint256;
     using Math for uint128;
     using SafeCast for uint256;
-    using Address for address;
     using SafeERC20 for IERC20;
 
     //////////////////////////////////////////////////////
