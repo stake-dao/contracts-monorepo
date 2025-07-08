ef8dcd3b93e4ee316d68df92a790fd01686d27b8
qd-qd <qdqdqdqdqd@protonmail.com>
2025-07-08 16:12:50 +0200

    script(strategies): morpho market deployments

---
+++ packages/strategies/script/integrations/morpho/03_DeployMorphoStableMarket.s.sol
@@ -0,0 +1,128 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.28;
+
+import {Script} from "forge-std/src/Script.sol";
+import {console} from "forge-std/src/console.sol";
+import {MorphoMarketFactory} from "src/integrations/morpho/MorphoMarketFactory.sol";
+import {Common} from "@address-book/src/CommonEthereum.sol";
+import {IRewardVault} from "src/interfaces/IRewardVault.sol";
+import {MarketParams, Id} from "@morpho/interfaces/IMorpho.sol";
+import {IMorphoOracle} from "src/interfaces/IMorphoOracle.sol";
+import {IMorphoStrategyWrapper} from "src/interfaces/IMorphoStrategyWrapper.sol";
+import {IChainlinkFeed} from "src/interfaces/IChainlinkFeed.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/interfaces/IERC20Metadata.sol";
+import {ICurvePool} from "src/interfaces/ICurvePool.sol";
+import {MarketParamsLib} from "@morpho/libraries/MarketParamsLib.sol";
+
+contract DeployMorphoStableMarketScript is Script {
+    function run() external returns (Id) {
+        address factory = vm.envAddress("MORPHO_MARKET_FACTORY");
+        address rewardVault = vm.envAddress("REWARD_VAULT");
+        address irm = vm.envOr("IRM", Common.MORPHO_ADAPTIVE_CURVE_IRM);
+        uint256 lltv = vm.envUint("LLTV");
+        address loanAsset = vm.envAddress("LOAN_ASSET");
+        address loanAssetFeed = vm.envAddress("LOAN_ASSET_FEED");
+        uint256 loanAssetFeedHeartbeat = vm.envUint("LOAN_ASSET_FEED_HEARTBEAT");
+        address baseFeed = vm.envOr("BASE_FEED", address(0));
+        uint256 baseFeedHeartbeat = vm.envOr("BASE_FEED_HEARTBEAT", uint256(0));
+
+        require(factory.code.length > 0, "MorphoMarketFactory not deployed");
+        require(rewardVault.code.length > 0, "RewardVault not deployed");
+        require(MorphoMarketFactory(factory).MORPHO_BLUE().isIrmEnabled(irm), "Invalid IRM");
+        require(MorphoMarketFactory(factory).MORPHO_BLUE().isLltvEnabled(lltv), "Invalid LLTV");
+        require(loanAsset != address(0), "Invalid loan asset");
+        require(loanAssetFeed.code.length > 0, "Invalid Loan Asset Feed");
+        require(loanAssetFeedHeartbeat > 0, "Invalid Loan Asset Heartbeat");
+        if (baseFeed != address(0)) {
+            require(baseFeed.code.length > 0, "Invalid Base Feed");
+            require(baseFeedHeartbeat > 0, "Invalid Base Feed Heartbeat");
+        }
+        require(
+            MorphoMarketFactory(factory).MORPHO_BLUE().isAuthorized(msg.sender, factory),
+            "Invalid Authorization. You must authorize the factory to manage your positions"
+        );
+
+        MorphoMarketFactory.MorphoMarketParams memory marketParams =
+            MorphoMarketFactory.MorphoMarketParams({loanAsset: loanAsset, irm: irm, lltv: lltv});
+
+        MorphoMarketFactory.StableswapOracleParams memory oracleParams = MorphoMarketFactory.StableswapOracleParams({
+            loanAssetFeed: loanAssetFeed,
+            loanAssetFeedHeartbeat: loanAssetFeedHeartbeat,
+            baseFeed: baseFeed,
+            baseFeedHeartbeat: baseFeedHeartbeat
+        });
+
+        _validationPrompt(IRewardVault(rewardVault), marketParams, oracleParams);
+
+        vm.startBroadcast();
+
+        (MarketParams memory morphoMarketParams, IMorphoOracle oracle, IMorphoStrategyWrapper wrapper) =
+            MorphoMarketFactory(factory).createStableswapMarket(IRewardVault(rewardVault), oracleParams, marketParams);
+
+        {
+            uint256 priceWei = oracle.price() / 10 ** (36 + IERC20Metadata(loanAsset).decimals() - wrapper.decimals());
+            console.log(
+                "The LP token is priced %d.%d %s.",
+                priceWei / 10 ** IERC20Metadata(loanAsset).decimals(),
+                priceWei % 10 ** IERC20Metadata(loanAsset).decimals(),
+                IERC20Metadata(loanAsset).symbol()
+            );
+            string memory answer = vm.prompt("Does it make sense to you? (y/n)");
+            if (
+                keccak256(bytes(answer)) != keccak256(bytes("y")) && keccak256(bytes(answer)) != keccak256(bytes("yes"))
+            ) {
+                revert("Deployment cancelled by user");
+            }
+
+            console.log("--------------------------------");
+            console.log("The market is deployed. You can now start borrowing from it.");
+            console.log("Oracle: %s", address(oracle));
+            console.log("Wrapper: %s", address(wrapper));
+            console.log("--------------------------------");
+        }
+
+        vm.stopBroadcast();
+
+        return MarketParamsLib.id(morphoMarketParams);
+    }
+
+    function _validationPrompt(
+        IRewardVault rewardVault,
+        MorphoMarketFactory.MorphoMarketParams memory marketParams,
+        MorphoMarketFactory.StableswapOracleParams memory oracleParams
+    ) internal {
+        console.log("--------------------------------");
+        console.log("You are about to deploy a Morpho Stable Market.");
+        console.log("Here's the parameters of the market you are about to deploy:");
+        console.log("- Reward Vault that will be wrapped: %s [%s]", address(rewardVault), rewardVault.name());
+        console.log("- Curve Pool: %s [%s]", rewardVault.asset(), IERC20Metadata(rewardVault.asset()).name());
+        console.log("- Loan Asset: %s [%s]", marketParams.loanAsset, IERC20Metadata(marketParams.loanAsset).name());
+        console.log("- IRM: %s", marketParams.irm);
+        console.log("- LLTV: %s", marketParams.lltv);
+        console.log("--------------------------------");
+        console.log(
+            "The first token of the Curve Pool is %s [%s]. The first provided feed must convert from this token",
+            ICurvePool(rewardVault.asset()).coins(0),
+            IERC20Metadata(ICurvePool(rewardVault.asset()).coins(0)).name()
+        );
+        console.log("--------------------------------");
+        console.log("The conversion from the LP token to the loan asset will follow this path:");
+        string memory paths = "";
+        if (oracleParams.baseFeed != address(0)) {
+            paths = string.concat(IChainlinkFeed(oracleParams.baseFeed).description(), " -> ");
+        }
+        paths = string.concat(IChainlinkFeed(oracleParams.loanAssetFeed).description());
+        console.log("Path: %s (last feed is reverted on purpose)", paths);
+        console.log("--------------------------------");
+        console.log(
+            "Please, double check the heartbeats and the feeds you provided here %s (%s)",
+            "https://docs.chain.link/data-feeds/price-feeds/addresses",
+            "Only Chainlink compliant oracles are supported"
+        );
+        console.log("--------------------------------");
+        string memory answer = vm.prompt("Are you sure you want to deploy this market? (y/n)");
+        if (keccak256(bytes(answer)) != keccak256(bytes("y")) && keccak256(bytes(answer)) != keccak256(bytes("yes"))) {
+            revert("Deployment cancelled by user");
+        }
+    }
+}
--- packages/strategies/src/interfaces/ICurvePool.sol
+++ packages/strategies/src/interfaces/ICurvePool.sol
@@ -1,17 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-interface ICurveStableSwapPool {
+interface ICurvePool {
+    function name() external view returns (string memory);
+    function decimals() external view returns (uint8);
+    function coins(uint256 index) external view returns (address);
+}
+
+interface ICurveStableSwapPool is ICurvePool {
     /// @dev Calculates the total value of all assets in the pool (using the StableSwap invariant)
     ///      and divides it by the total supply of LP tokens. The returned value is scaled up by 1e18.
     ///      The value is always denominated in the pool's "unit of account" (e.g., USD for a DAI/USDC/USDT pool).
     function get_virtual_price() external view returns (uint256);
-    function name() external view returns (string memory);
-    function decimals() external view returns (uint8);
 }
 
-interface ICurveCryptoSwapPool {
+interface ICurveCryptoSwapPool is ICurvePool {
     function lp_price() external view returns (uint256);
-    function name() external view returns (string memory);
-    function decimals() external view returns (uint8);
 }
