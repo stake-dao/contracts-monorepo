09699b49080e9c8873de3102aefeef34ebfe2f1a
qd-qd <qdqdqdqdqd@protonmail.com>
2025-07-08 14:06:14 +0200

    test(strategies): morpho market factory e2e tests

--- packages/address-book/src/CommonEthereum.sol
+++ packages/address-book/src/CommonEthereum.sol
@@ -5,6 +5,7 @@ import {CommonUniversal} from "./CommonUniversal.sol";
 
 library Common {
     address internal constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address internal constant USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
     address internal constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address internal constant WSTETH = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
     address internal constant WBTC = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
@@ -30,6 +31,7 @@ library Common {
 
     // Chainlink
     address internal constant CHAINLINK_USDC_USD_PRICE_FEED = 0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6;
+    address internal constant CHAINLINK_USDT_USD_PRICE_FEED = 0x3E7d1eAB13ad0104d2750B8863b489D65364e32D;
     address internal constant CHAINLINK_CRVUSD_USD_PRICE_FEED = 0xEEf0C605546958c1f899b6fB336C20671f9cD49F;
     address internal constant CHAINLINK_ETH_USD_PRICE_FEED = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419;
     address internal constant CHAINLINK_CRV_USD_PRICE_FEED = 0xCd627aA160A6fA45Eb793D19Ef54f5062F20f33f;
--- packages/strategies/test/integration/curve/mainnet/CurveMainnetIntegration.t.sol
+++ packages/strategies/test/integration/curve/mainnet/CurveMainnetIntegration.t.sol
@@ -4,36 +4,34 @@ pragma solidity ^0.8.27;
 import "test/integration/curve/CurveIntegration.sol";
 import {CurveLocker, CurveProtocol} from "@address-book/src/CurveEthereum.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
-import {ConvexSidecar} from "src/integrations/curve/ConvexSidecar.sol";
-import {ConvexSidecarFactory} from "src/integrations/curve/ConvexSidecarFactory.sol";
-import {OnlyBoostAllocator} from "src/integrations/curve/OnlyBoostAllocator.sol";
 import {ILiquidityGauge} from "@interfaces/curve/ILiquidityGauge.sol";
 import {IMinter} from "@interfaces/curve/IMinter.sol";
-import {IBaseRewardPool} from "@interfaces/convex/IBaseRewardPool.sol";
 
 contract CurveMainnetIntegrationTest is CurveIntegration {
     using Math for uint256;
 
-    Config public _config = Config({
-        base: BaseConfig({
-            chain: "mainnet",
-            blockNumber: 22_316_395,
-            rewardToken: CurveProtocol.CRV,
-            locker: CurveLocker.LOCKER,
-            protocolId: bytes4(keccak256("CURVE")),
-            harvestPolicy: IStrategy.HarvestPolicy.CHECKPOINT,
-            minter: CurveProtocol.MINTER,
-            boostProvider: CurveProtocol.VE_BOOST,
-            gaugeController: CurveProtocol.GAUGE_CONTROLLER,
-            oldStrategy: CurveLocker.STRATEGY
-        }),
-        convex: ConvexConfig({
-            isOnlyBoost: true,
-            cvx: CurveProtocol.CONVEX_TOKEN,
-            convexBoostHolder: CurveProtocol.CONVEX_PROXY,
-            booster: CurveProtocol.CONVEX_BOOSTER
-        })
-    });
+    function getConfig() internal view virtual returns (Config memory) {
+        return Config({
+            base: BaseConfig({
+                chain: "mainnet",
+                blockNumber: 22_311_339,
+                rewardToken: CurveProtocol.CRV,
+                locker: CurveLocker.LOCKER,
+                protocolId: bytes4(keccak256("CURVE")),
+                harvestPolicy: IStrategy.HarvestPolicy.CHECKPOINT,
+                minter: CurveProtocol.MINTER,
+                boostProvider: CurveProtocol.VE_BOOST,
+                gaugeController: CurveProtocol.GAUGE_CONTROLLER,
+                oldStrategy: CurveLocker.STRATEGY
+            }),
+            convex: ConvexConfig({
+                isOnlyBoost: true,
+                cvx: CurveProtocol.CONVEX_TOKEN,
+                convexBoostHolder: CurveProtocol.CONVEX_BOOSTER,
+                booster: CurveProtocol.CONVEX_BOOSTER
+            })
+        });
+    }
 
     function poolIds() public view virtual returns (uint256[] memory) {
         uint256[] memory _poolIds = new uint256[](7);
@@ -50,7 +48,16 @@ contract CurveMainnetIntegrationTest is CurveIntegration {
     // All pool IDs from the old tests
     // uint256[] public poolIds = [68, 40, 437, 436, 435, 434, 433];
 
-    constructor() CurveIntegration(_config) {}
+    constructor() CurveIntegration(getConfig()) {
+        vm.label(CurveProtocol.CRV, "CRV");
+        vm.label(CurveLocker.LOCKER, "Locker");
+        vm.label(CurveProtocol.MINTER, "Minter");
+        vm.label(CurveProtocol.VE_BOOST, "VE Boost");
+        vm.label(CurveProtocol.GAUGE_CONTROLLER, "Gauge Controller");
+        vm.label(CurveLocker.STRATEGY, "Strategy");
+        vm.label(CurveProtocol.CONVEX_TOKEN, "CVX");
+        vm.label(CurveProtocol.CONVEX_BOOSTER, "Booster");
+    }
 
     function deployRewardVaults()
         internal
