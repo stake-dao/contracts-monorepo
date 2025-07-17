e305d81b2dea57e2d2a1566aaa21a97aab860cdd
Warren <99427649+kobe-eth@users.noreply.github.com>
2025-07-17 13:16:17 +0200

    release: deploy resolv prelaunch locker

--- packages/address-book/src/ResolvEthereum.sol
+++ packages/address-book/src/ResolvEthereum.sol
@@ -4,3 +4,12 @@ pragma solidity >=0.8.0;
 library ResolvProtocol {
     address internal constant RESOLV = 0x259338656198eC7A76c729514D3CB45Dfbf768A1;
 }
+
+library ResolvLocker {
+    address internal constant SDRESOLV = 0x58AAd88516bD26439e68BFB1DEAD78538Ab59fFd;
+
+    address internal constant LOCKER = 0x64fd7a4b0B43F47527232392C1Ea633826355107;
+    address internal constant PRELAUNCH_LOCKER = 0x5C5ccB2E511FcdeAa16737810787a47507Cf0922;
+
+    address internal constant GAUGE = 0xf54bCEED6396E4698cD688eab5B58b86cca0F68d;
+}
