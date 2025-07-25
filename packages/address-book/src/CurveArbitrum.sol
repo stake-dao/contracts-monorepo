a047d201bca28956319b877f0899cd172aa67989
Warren <99427649+kobe-eth@users.noreply.github.com>
2025-07-25 14:06:47 +0200

    release: arbitrum deployment

--- packages/address-book/src/CurveArbitrum.sol
+++ packages/address-book/src/CurveArbitrum.sol
@@ -15,3 +15,20 @@ library CurveProtocol {
 library CurveVotemarket {
     address internal constant PLATFORM = 0x8c2c5A295450DDFf4CB360cA73FCCC12243D14D9;
 }
+
+library CurveStrategy {
+    address internal constant ACCOUNTANT = 0x93b4B9bd266fFA8AF68e39EDFa8cFe2A62011Ce0;
+    address internal constant PROTOCOL_CONTROLLER = 0x2d8BcE1FaE00a959354aCD9eBf9174337A64d4fb;
+    address internal constant GATEWAY = 0xe5d6D047DF95c6627326465cB27B64A8b77A8b91;
+
+    address internal constant STRATEGY = 0x021C3A83333a51d3c956F5c9748C384c897C8E14;
+
+    address internal constant CONVEX_SIDECAR_IMPLEMENTATION = 0x735A969463967578Fcc17cEB9bba32893d00f71d;
+    address internal constant CONVEX_SIDECAR_FACTORY = 0xf368a89e1731b9362670786d36866910c5334477;
+
+    address internal constant FACTORY = 0x83922F1188Bd7661921b1FC02616F65B1DfA2092;
+    address internal constant ALLOCATOR = 0x6Dbf307916Ae9c47549AbaF11Cb476252a14Ee9D;
+
+    address internal constant REWARD_VAULT_IMPLEMENTATION = 0x74D8dd40118B13B210D0a1639141cE4458CAe0c0;
+    address internal constant REWARD_RECEIVER_IMPLEMENTATION = 0x8E57D90Ef325F2BeDf86333be784b4499f8829df;
+}
